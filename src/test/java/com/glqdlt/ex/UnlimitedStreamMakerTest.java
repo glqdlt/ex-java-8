97237d799b7bf5a4e3624eb3544ced3168ec2405
IwJhun <dlfdnd0725@gmail.com>
2018-05-03 18:52:42 +0900

    무한 스트림

--- src/test/java/com/glqdlt/ex/UnlimitedStreamMakerTest.java
+++ src/test/java/com/glqdlt/ex/UnlimitedStreamMakerTest.java
@@ -29,19 +29,27 @@ public class UnlimitedStreamMakerTest {
 
 //      극혐 코드, 와 몇 줄이야 ㅅㅂ
         List<Integer> monolothicNumbers = new ArrayList<>();
-        for (int i =0; i< 100; i++){
+        for (int i = 0; i < 100; i++) {
             monolothicNumbers.add(i);
         }
         monolothicNumbers.forEach(System.out::println);
 
 //        아래 처럼 ++x 로 해도되는데 가독성이 더 구려서..
 //        List<Integer> someNumbers = Stream.iterate(0, x -> ++x)
+//        iterate 로 할 경우에 limit가 있더라도, 우선 무한 스트림을 만든다. 이를 보고 언바운드 스트림이라고 표현하는듯.
         List<Integer> someNumbers = Stream.iterate(0, x -> x + 1).limit(100).peek(System.out::println).collect(toList());
-
 //        아래처럼 IntStream의 rangeCLosed를 활용해서 할 수도 있다. foreach 로 찍기 보단 peek을 쓰는 게 참 편하네..
-        List<Integer> someNumbersAsIntStream = IntStream.rangeClosed(0,100).boxed().peek(System.out::println).collect(toList());
+        List<Integer> someNumbersAsIntStream = IntStream.rangeClosed(0, 100).boxed().peek(System.out::println).collect(toList());
 
 
+    }
+
+    @Test
+    public void pibonachi() {
+        Stream.iterate(new int[]{0, 1}, x -> new int[]{x[1], x[0] + x[1]})
+                .limit(10)
+                .peek(x -> System.out.println(x[0] + "," + x[1]))
+                .collect(toList());
     }
 
 }
